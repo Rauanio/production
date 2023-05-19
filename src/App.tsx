3c2a8058ee7f6c2e615f18fa5577a5f1e8d4fc7c
Rauanio <rauanovich01@mail.ru>
2023-05-19 18:19:43 +0600

    text

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { Link } from 'react-router-dom';
+import { Suspense } from 'react';
+import { useTheme } from './theme/useTheme';
+import './styles/index.scss';
+import { classNames } from './helpers/classNames/classNames';
+
+export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={classNames('app', {}, [theme])}>
+      <button onClick={toggleTheme}>Toggle</button>
+      <Link to="/">Главная</Link>
+      <Link to="/about">О нас</Link>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
