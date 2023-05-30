e4c2f85c3f67ab1ffd11575fe6aaaa61f5424e39
Rauanio <rauanovich01@mail.ru>
2023-05-30 22:54:31 +0600

    fix error

--- src/pages/NotFoundPage/ui/NotFoundPage.stories.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.stories.tsx
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-=======
-import type { Meta, StoryObj } from '@storybook/react';
->>>>>>> ebb6aaa756a5865b8934b0eba800db297368387c
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
 import { NotFoundPage } from './NotFoundPage';
 
-<<<<<<< HEAD
 export default {
     title: 'pages/NotFoundPage',
     component: NotFoundPage,
@@ -23,23 +18,4 @@ Light.args = {};
 
 export const Dark = Template.bind({});
 Dark.args = {};
-=======
-const meta: Meta<typeof NotFoundPage> = {
-    title: 'pages/NotFoundPage',
-    component: NotFoundPage,
-    decorators: [ThemeDecorator(Theme.LIGHT)],
-};
-
-export default meta;
-type Story = StoryObj<typeof NotFoundPage>;
-
-export const Light: Story = {
-    args: {},
-};
-
-export const Dark: Story = {
-    args: {},
-};
-
->>>>>>> ebb6aaa756a5865b8934b0eba800db297368387c
 Dark.decorators = [ThemeDecorator(Theme.DARK)];
