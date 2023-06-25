9667c1aead45746adb90c601efd301cc1b082f94
Rauanio <rauanovich01@mail.ru>
2023-06-25 17:47:40 +0600

    add private protected routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,15 @@ import React, { Suspense } from 'react';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { useDispatch } from 'react-redux';
-import { userActions } from 'entities/user';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserInited, userActions } from 'entities/user';
 import { AppRouter } from './providers/router';
 import { classNames } from '../shared/lib/classNames/classNames';
 
 export function App() {
     const { theme } = useTheme();
     const dispatch = useDispatch();
+    const inited = useSelector(getUserInited);
 
     React.useEffect(() => {
         dispatch(userActions.initAuthData());
@@ -21,7 +22,7 @@ export function App() {
                 <Navbar />
                 <div className="content-page">
                     <Sidebar />
-                    <AppRouter />
+                    {inited && <AppRouter />}
                 </div>
             </Suspense>
         </div>
