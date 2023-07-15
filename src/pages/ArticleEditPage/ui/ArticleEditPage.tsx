15c72b691adf74a7902ceba81839dd81940f7cdb
Rauanio <rauanovich01@mail.ru>
2023-07-15 17:29:53 +0600

    production

--- src/pages/ArticleEditPage/ui/ArticleEditPage.tsx
+++ src/pages/ArticleEditPage/ui/ArticleEditPage.tsx
@@ -3,7 +3,6 @@ import { useTranslation } from 'react-i18next';
 import { ReactNode, memo } from 'react';
 import { useParams } from 'react-router-dom';
 import { Wrapper } from 'widgets/Wrapper/Wrapper';
-import cls from './ArticleEditPage.module.scss';
 
 export interface ArticleEditPageProps {
     className?: string;
@@ -15,7 +14,7 @@ const ArticleEditPage = ({ className }: ArticleEditPageProps) => {
     const canEdit = Boolean(id);
 
     return (
-        <Wrapper className={classNames(cls.articleEditPage, {}, [className])}>
+        <Wrapper className={classNames('', {}, [className])}>
             {canEdit ? t(`Редактирование статьи с ID ${id}`) : t('Создание статьи') }
         </Wrapper>
     );
