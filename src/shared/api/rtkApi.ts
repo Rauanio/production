773017bc8773a63256ffd3413918997e14611123
Rauanio <rauanovich01@mail.ru>
2023-08-25 16:59:24 +0600

    fix netlify error

--- src/shared/api/rtkApi.ts
+++ src/shared/api/rtkApi.ts
@@ -1,12 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
+
+export const USER_LOCALSTORAGE_KEY = 'user';
 
 export const rtkApi = createApi({
     reducerPath: 'rtkApi',
     baseQuery: fetchBaseQuery({
         baseUrl: __API__,
         prepareHeaders: (headers) => {
-            const token = localStorage.getItem('user') || '';
+            const token = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
             if (token) {
                 headers.set('Authorization', token);
             }
